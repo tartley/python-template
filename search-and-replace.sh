5de7a501d80f735eff4ddcaa510d7adac40c79d3
Jonathan Hartley <tartley@tartley.com>
2015-10-02 19:46:27 +0100

    simplify s&r

--- search-and-replace.sh
+++ search-and-replace.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-USAGE="\
-Usage: $0 <search> <replace>
+USAGE="Usage: $(basename $0) <search> <replace>
 
 Search-and-replace in all files under current directory (recursively)
-both in file content and in file and directory names.
+File content is modified in place. We skip the .git directory.
 
-We use a quick hack to skip '.git' directories, so as not to break the
-repo, but in doing so we also skip all top level dot files (e.g. .gitignore)
+Also lists files and directories which contain <search> in the name,
+so the user can manually rename them if they wish.
 "
 
 # set -x # exit on error
@@ -17,46 +16,19 @@ SEARCH=$1
 REPLACE=$2
 DEBUG=$DEBUG
 
-function log {
-  if [[ $DEBUG ]]; then
-    echo "$1"
-  fi
-}
-
 if [ -z "$REPLACE" ] ; then
-    echo $USAGE
+    echo -e >&2 "$USAGE"
     exit 2
 fi
 
 # Search-and-replace in file content
-log "Replacing file content:"
+echo "Replacing file content:"
 command='grep -lr --exclude-dir .git "$SEARCH" .'
 for filename in $(eval $command); do
-    log "  $filename"
+    echo "  $filename"
     sed -i s/$SEARCH/$REPLACE/g "$filename"
 done
 
-# Search-and-replace in directory and file names.
-# Using '*' instead of '.' skips the .git & .idea directories.
-# We do this in two loops, dirs followed by files, so that files of the form:
-#   xxx/xxxSEARCHxxx/xxx/xxxSEARCHxxx
-# have their directory renamed by the first iteration,
-# and their base file name renamed by the second iteration.
-# avoiding the file-not-found errors that occur otherwise.
-# This will break if we start using directories with more than one occurence of
-# the search term, e.g:
-#   xxx/xxxSEARCHxxx/xxx/xxxSEARCHxxx/xxx
-log "Renaming dirs:"
-command='find * -type d -name "*$SEARCH*"'
-for filename in $(eval $command); do
-  log "  $filename"
-  mv "$filename" "${filename/$SEARCH/$REPLACE}"
-done
-
-log "Renaming files:"
-command='find * -type f -name "*$SEARCH*"'
-for filename in $(eval $command); do
-  log "  $filename"
-  mv "$filename" "${filename/$SEARCH/$REPLACE}"
-done
+echo "The following file or dir names contain $SEARCH:"
+find * -name "*$SEARCH*"
 
