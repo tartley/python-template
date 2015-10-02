aa9ef624e13a3d43fd61055f0fa5b0253fa3212a
Jonathan Hartley <tartley@tartley.com>
2015-10-02 16:42:30 +0100

    A working python app/lib template.
    Cmdline --help and --version work, thanks to docopt.
    Makefile contains commands to upload to PyPI.
    tests pass.

---
+++ PROJNAME/__init__.py
@@ -0,0 +1,18 @@
+'''
+PROJNAME v{version}
+
+Short description.
+
+Usage:
+    PROJNAME [options]
+
+    --help      Show this text.
+    --version   Print version number and exit.
+
+For details, see the README file.
+'''
+
+__version__ = '0.0.1'
+
+__doc__ = __doc__.format(version=__version__)
+
